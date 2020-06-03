8c07606567f94b6a65879188e28dfd6f4c858973
Renato <otonatoh@gmail.com>
2020-06-03 13:54:54 -0300

    acrescentado uma mensagem de bom dia

--- Projeto_teste/Program.cs
+++ Projeto_teste/Program.cs
@@ -4,6 +4,7 @@ namespace Projeto_teste {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("bom dia"  );
         }
     }
 }
